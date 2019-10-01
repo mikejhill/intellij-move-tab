81a68c40c5e07ad170b4da17c7980cefb06b08c7
Mike Hill <hill0826@umn.edu>
2019-10-01 10:23:55 -0500

    Add project structure

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://dl.bintray.com/jetbrains/intellij-plugin-service")
+        }
+    }
+}
+
+repositories {
+    jcenter()
+}
+
+plugins {
+    base
+    `kotlin-dsl`
+    id("org.jetbrains.intellij") version "0.4.10"
+}
+
+group = "com.mikejhill"
+version = "1.0.0"
+
+intellij {
+    version = "2019.2.3"
+    type = "IU"
+}
+
+tasks.withType<Wrapper> {
+    distributionType = Wrapper.DistributionType.ALL
+}
