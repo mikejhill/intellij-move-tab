17e3b7b6bef3d019baedddb68d5584ffc95a6afd
Mike Hill <mike@mikejhill.com>
2025-06-05 23:21:30 -0400

    Update Develocity plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.19.2"
+    id("com.gradle.develocity") version "4.0.2"
     id("com.gradle.common-custom-user-data-gradle-plugin") version "2.3"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
@@ -15,13 +15,13 @@ rootProject.name = "intellij-move-tab"
 
 include("move-tab-plugin")
 
-gradleEnterprise {
+develocity {
     buildScan {
-        capture.isTaskInputFiles = true
-        isUploadInBackground = true
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-        publishAlways()
+        capture { fileFingerprints.set(true) }
+        uploadInBackground.set(true)
+        termsOfUseUrl.set("https://gradle.com/terms-of-service")
+        termsOfUseAgree.set("yes")
+        publishing.onlyIf { true }
         obfuscation {
             hostname { "<obfuscated>" }
             ipAddresses { listOf("<obfuscated>") }
