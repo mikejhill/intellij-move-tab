39ea5815711c2f716ddbe3d7d7598d7642b95679
Mike Hill <mike@mikejhill.com>
2025-06-05 21:05:36 -0400

    Revert wrapper script changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,9 @@ pluginManagement {
 }
 
 plugins {
-    id("com.gradle.enterprise") version "3.15.1"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.11.3"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("com.gradle.enterprise") version "3.19.2"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.3"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "intellij-move-tab"
