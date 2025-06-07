2d897242ae998a38687e143582bcb63aa6e549d4
Mike Hill <mike@mikejhill.com>
2025-06-07 13:35:54 -0400

    update(framework): update to IntelliJ Platform Gradle Plugin 2.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,26 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.intellij.platform.gradle.tasks.PublishPluginTask
+import org.jetbrains.intellij.platform.gradle.tasks.RunIdeTask
+
 plugins {
+    id("org.jetbrains.intellij.platform")
 }
 
-allprojects {
-    repositories {
-        mavenCentral()
+val publishPlugin by tasks.existing(PublishPluginTask::class) {
+    token.set(getPropertyValue("publishToken"))
+}
+
+tasks.withType<RunIdeTask> {
+    jvmArgs("-Xms4g", "-Xmx4g", "-Dcom.sun.management.jmxremote")
+}
+
+fun getPropertyValue(name: String): String? {
+    return if (project.extra.has(name)) project.extra[name]?.toString() else System.getenv(name)
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2025.1.2")
+        pluginModule(implementation(project(":move-tab-plugin")))
     }
 }
--- move-tab-plugin/build.gradle.kts
+++ move-tab-plugin/build.gradle.kts
@@ -1,22 +1,13 @@
-import org.jetbrains.intellij.tasks.PublishPluginTask
-import org.jetbrains.intellij.tasks.RunIdeTask
-
-repositories {
-    mavenCentral()
-    intellijPlatform {
-        defaultRepositories()
-    }
-}
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
     id("java")
     kotlin("jvm") version "2.2.0-RC2"
-    id("org.jetbrains.intellij.platform") version "2.6.0"
+    id("org.jetbrains.intellij.platform.module")
 }
 
 group = "com.mikejhill"
 version = "2.2.0"
-
 
 java {
     toolchain {
@@ -29,35 +20,20 @@ kotlin {
 }
 
 intellijPlatform {
-    dependencies {
-        create("IC", "251.26094.121")
-    }
     pluginConfiguration {
-        ideaVersion {
-            sinceBuild = "251"
-        }
         changeNotes = project.provider { rootProject.file("docs/CHANGELOG.html").readText() }
     }
-}
-
-val publishPlugin by tasks.existing(PublishPluginTask::class) {
-    token.set(getPropertyValue("publishToken"))
-}
-
-tasks.withType<RunIdeTask> {
-    jvmArgs("-Xms4g", "-Xmx4g", "-Dcom.sun.management.jmxremote")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-fun getPropertyValue(name: String): String? {
-    return if (project.extra.has(name)) project.extra[name]?.toString() else System.getenv(name)
-}
-
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.13.0")
+    intellijPlatform {
+        intellijIdeaCommunity("2025.1.2")
+        testFramework(TestFrameworkType.Platform)
+    }
     testImplementation("io.mockk:mockk:1.14.2")
     // Required for BasePlatformTestCase (see FAQ about JUnit5 tests referring to JUnit4
     // https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-faq.html#junit5-test-framework-refers-to-junit4)
